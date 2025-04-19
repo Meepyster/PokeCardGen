47854417806eb574bd76a0dd956e4756cee8c68e
= <=>
2025-04-19 07:56:21 +0000

    added tests

---
+++ test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get10Cards():
+    """Simple Sample Test"""
+    response = client.get("/get-10-cards")
+    assert response.status_code == 200
+    data = response.json()
+    assert len(data) == 10
