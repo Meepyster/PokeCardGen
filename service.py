cc43038879cc768882015674460d69e99a3c69f0
= <=>
2025-09-28 04:28:58 +0000

    fixed upgraded card titles

--- service.py
+++ service.py
@@ -31,7 +31,7 @@ def upgrade_card(card, set_config, cards_by_rarity):
     for target_rarity, chance in sorted(upgrade_options.items(), key=lambda x: -x[1]):
         if random.random() < chance and cards_by_rarity.get(target_rarity):
             print("\nUPGRADE TO: ", target_rarity, "\n")
-            return random.choice(cards_by_rarity[target_rarity])
+            card = random.choice(cards_by_rarity[target_rarity])
     card["card_title"] = f"{card["rarity"]} {card["name"]}"
     return card
 
